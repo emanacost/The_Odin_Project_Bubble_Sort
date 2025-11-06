dc04da40eb76a9523e0ce0372047c6c87ae67aa0
eacosta <eacosta0411@gmail.com>
2025-11-06 15:01:22 -0500

    Add basic skeleton to bubble_method sort

--- Bubble_Sort.rb
+++ Bubble_Sort.rb
@@ -1,5 +1,17 @@
-def bubble_sort
-
+def bubble_sort(arr)
+  n = 0
+  while n <= arr.length
+    swapped = false
+    arr.each_with_index do |index, val|
+      if arr[index] > arr.fetch(index + 1)
+        arr[index], arr.fetch(index + 1) = arr.fetch(index + 1), arr[index]
+        swapped = true
+      end
+    end
+    if swapped = false
+      break
+    end  
+  end
 end
 
 
