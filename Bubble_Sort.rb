cc4d9bd802dda5ea3d06d6375661e32d727ceadc
eacosta <eacosta0411@gmail.com>
2025-11-06 21:58:35 -0500

    Fix array loop logic and while loop condition

--- Bubble_Sort.rb
+++ Bubble_Sort.rb
@@ -1,10 +1,11 @@
 def bubble_sort(arr)
   n = 0
-  while n <= arr.length - 1 do
+  while n < arr.length do
     swapped = false
-    arr.each_with_index do |index, val|
-      if arr[index] > arr.fetch(index + 1)
-        arr[index], arr.fetch(index + 1) = arr.fetch(index + 1), arr[index]
+    arr.each_with_index do |val, index|
+      next if index == 0
+      if arr.fetch(index - 1) > arr.fetch(index)
+        arr[index - 1], arr[index] = arr[index], arr[index - 1]
         swapped = true
       end
     end
@@ -19,3 +20,7 @@ end
 
 
 # bubble_sort([[4,3,78,2,0,2]]) => [0,2,2,3,4,78]
+
+puts bubble_sort([4,3,78,2,0,2]).join(",")
+
+puts bubble_sort([5, 4, 3, 2, 1]).join(",")
